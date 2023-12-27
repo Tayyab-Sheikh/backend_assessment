256dd099734cb4e2fb40d1137890ec9e694c943e
Tayyab-Sheikh <tayabsheikh900@gmail.com>
2023-12-27 14:43:14 +0500

    add kafka process message implementation

--- backend/social/utilities/StreamManager.js
+++ backend/social/utilities/StreamManager.js
@@ -1,7 +1,42 @@
+const { storeTenant } = require("../services/tenant-profile.services");
+const { storeUser } = require("../services/user-profile.services");
+
 const processMessage = async (kafkaMessage) => {
 
-	//Start working here
-	console.log(kafkaMessage);
+	try {
+		//Start working here
+		const { event_name } = kafkaMessage;
+
+		if (event_name == "tenant_created") {
+
+			let tenantData = { ...kafkaMessage.properties, tenant_id: kafkaMessage.properties.id };
+
+			delete tenantData.id
+
+			const response = await storeTenant(tenantData);
+
+			if (response.statusCode == 200) {
+				console.log(`Tenant Created!`)
+			}
+
+		} else if (event_name == "user_created") {
+
+			let userData = { ...kafkaMessage.properties, user_id: kafkaMessage.properties.id }
+			delete userData.id
+
+			const response = await storeUser(userData);
+
+			if (response.statusCode == 200) {
+				console.log(`User Created!`)
+			}
+
+		}
+
+	} catch (error) {
+
+		console.log("Error processing Kafka message:", error);
+
+	}
 
 };
 
